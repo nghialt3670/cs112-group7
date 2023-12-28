b7a2ebae0f32a7c5719eadf31bba2e66d6aa6150
nghialt3670 <nghialt3670@gmail.com>
2023-12-28 21:12:45 +0700

    update solution for problem 2

--- wecode_exercises/solutions/problem_2.py
+++ wecode_exercises/solutions/problem_2.py
@@ -1,25 +1,32 @@
+# Read input values N and M
 N, M = map(int, input().split())
-trees = []
 
+# Initialize lists to store tree and cylinder coordinates
+trees = []
+cylinders = []
+
+# Read tree coordinates and append to the 'trees' list
 for i in range(N):
     x, y = map(int, input().split())
     trees.append([x, y])
 
-cylinders = []
+# Read cylinder coordinates and append to the 'cylinders' list
 for i in range(M):
     x, y = map(int, input().split())
     cylinders.append([x, y])
 
+# Function to determine orientation of three points (p, q, r)
 def orientation(p, q, r):
     val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
     if val == 0:
-        return 0
-    return 1 if val > 0 else -1
+        return 0  # Collinear points
+    return 1 if val > 0 else -1  # Clockwise or counterclockwise
 
+# Function to compute the convex hull using the Monotone Chain algorithm
 def monotone_chain(points):
     n = len(points)
     if n < 3:
-        return points
+        return points  # Convex hull is not possible with less than 3 points
 
     # Sort the points lexicographically
     points = sorted(points)
@@ -38,15 +45,17 @@ def monotone_chain(points):
             upper_hull.pop()
         upper_hull.append(p)
 
-    # Concatenate lower and upper hulls
+    # Concatenate lower and upper hulls to get the convex hull
     convex_hull = lower_hull[:-1] + upper_hull[:-1]
 
     return convex_hull
 
-
+# Compute the convex hull of cylinders using the Monotone Chain algorithm
 convex_hull = monotone_chain(cylinders)
+
+# Sort the convex hull points lexicographically
 convex_hull = sorted(convex_hull, key=lambda point: (point[0], point[1]))
 
+# Print the convex hull points
 for x, y in convex_hull:
     print(x, y)
-
